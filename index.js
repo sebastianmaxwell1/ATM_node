380ddfb6f66e3bc6e21ad37c3feb280b0d991c36
Sebastian Maxwell <sebastianrmaxwell@gmail.com>
2021-10-14 22:33:24 -0400

    Create account / atm files

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log('hello');
+const prompt = require('prompt-sync')();
+
+console.log('Deposit or Withdrawl');
+
+const atmAction = prompt();
+
+console.log(`You are going to make a ${atmAction}!`)
